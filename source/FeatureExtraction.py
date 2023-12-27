697a12870192c92bb9524db6296dedb35e573bbf
pgtrungg <pgtrungg@gmail.com>
2023-12-27 14:05:45 +0700

    fix feature extraction

--- source/FeatureExtraction.py
+++ source/FeatureExtraction.py
@@ -1,44 +1,45 @@
 import cv2
 import numpy as np
-from scipy.ndimage import convolve
+from sklearn.preprocessing import MinMaxScaler
 from ImageEnhancement import image_enhancement
 
 def apply_gabor_filters(image):
     image=image_enhancement(image)
-    # Set Gabor filter parameters
-    orientations = [0, np.pi/4, np.pi/2, 3*np.pi/4]
-    frequencies = [0.1, 0.5, 1, 2]
-    # Convert the image to grayscale if it's not already
-    if len(image.shape) == 3:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Define Gabor filter parameters
+    ksize = 21  # Filter size
+    sigma = 5.0  # Standard deviation of the Gaussian
+    theta = 0.0  # Orientation of the filter
+    lambda_val = 10.0  # Wavelength of the sinusoidal function
+    gamma = 0.5  # Spatial aspect ratio
 
-    # Normalize pixel values to be in the range [0, 1]
-    image = image / 255.0
+    # Create Gabor filter
+    gabor_filter = cv2.getGaborKernel((ksize, ksize), sigma, theta, lambda_val, gamma)
 
-    features = []
+    # Convolve the iris image with the Gabor filter
+    filtered_image = cv2.filter2D(image, cv2.CV_64F, gabor_filter)
 
-    for frequency in frequencies:
-        for theta in orientations:
-            # Create Gabor filter kernel
-            kernel = cv2.getGaborKernel((21, 21), sigma=5, theta=theta, lambd=10, gamma=0.5, psi=0)
+    # Calculate the phase of the filtered image
+    phase_image = np.angle(filtered_image)
 
-            # Convolve the image with the Gabor kernel
-            gabor_filtered = convolve(image, kernel, mode='constant', cval=0.0)
-
-            # Extract feature statistics (mean and variance) from the filtered image
-            mean = np.mean(gabor_filtered)
-            variance = np.var(gabor_filtered)
-
-            features.extend([mean, variance])
-
-    return features
+    # Convert phase information to a binary code
+    binary_iris_code = (phase_image > 0).astype(np.uint8)
+    return binary_iris_code
 
 #MAIN
-input_path="dataset\\1\\left\\aeval1.bmp"
+input_path="dataset\\46\\left\\zulaikahl1.bmp"
 img=cv2.imread(input_path)
 
 # Apply Gabor filters and get features
-gabor_features = apply_gabor_filters(img)
+gabor_features1 = apply_gabor_filters(img)
 
-# Print or use the extracted features as needed
-print("Gabor Features:", gabor_features)
+input_path="dataset\\46\\left\\zulaikahl2.bmp"
+img=cv2.imread(input_path)
+
+# Apply Gabor filters and get features
+gabor_features2 = apply_gabor_filters(img)
+
+cv2.imshow("1",gabor_features1*255)
+cv2.imshow("2",gabor_features2*255)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
